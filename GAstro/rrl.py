390f6e54da7ed2bb908bf0aec7345758f7b8c769
Giuliano Iorio <giuliano.iorio89@gmail.com>
2019-07-02 13:28:18 +0100

    added functions in rrl.py

--- GAstro/rrl.py
+++ GAstro/rrl.py
@@ -126,9 +126,9 @@ def is_HASP(period, amp, offset=-0.15, period_range=(0.43,0.48), amp_range=(0.9,
 	:param period: rrl period [days]
 	:param amp:  rrl amplitude (peak_to_peak)
 	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
-	:param period_range: extra cut on period range. If not None, it need to be a tuple with two values. Only stars
+	:param period_range: period box to be used to select HASP RRL. If not None, it need to be a tuple with two values. Only stars
 	within these values will be taken into account for the classification.
-	:param amp_range: extra cut on amp range. If not None, it need to be a tuple with two values. Only stars
+	:param amp_range: amp box to be used to select HASP RRL. If not None, it need to be a tuple with two values. Only stars
 	within these values will be taken into account for the classification.
 	:return: A boolean array with the same len of period and amp with True where the stars are in the given period-amplitude box.
 	"""
@@ -139,3 +139,94 @@ def is_HASP(period, amp, offset=-0.15, period_range=(0.43,0.48), amp_range=(0.9,
 
 	return idx_general
 
+def f_type1(period, amp, offset=-0.15, extra_cut=True, period_range=(0.43, 0.9), amp_range=None, consider_all=False):
+	"""
+	Estimate the fraction of Type1 RRL stars (Bases on the classification by Belokurov+18 (MNRAS.477.1472B) )
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range: extra cut on period range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range: extra cut on amp range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param consider_all: if True, the fraction is N1/Ntot where Ntot is the len of the period array in input. If
+	False the fraction is N1/(N1+N2) so we do not consider the number of objects that have not been classified.
+	:return:  fraction of Type1 RRL stars.
+	"""
+
+	classification_array=classify_type12(period, amp, offset=offset, extra_cut=extra_cut, period_range=period_range, amp_range=amp_range)
+	N1 = np.sum(classification_array == 1)
+	if consider_all:
+		frac = N1/len(period)
+	else:
+		N2 = np.sum(classification_array==2)
+		Ntot = N1+N2
+		frac = N1 / Ntot
+
+	return frac
+
+
+def f_type2(period, amp, offset=-0.15, extra_cut=True, period_range=(0.43, 0.9), amp_range=None, consider_all=False):
+	"""
+	Estimate the fraction of Type2 RRL stars (Bases on the classification by Belokurov+18 (MNRAS.477.1472B) )
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range: extra cut on period range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range: extra cut on amp range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param consider_all: if True, the fraction is N2/Ntot where Ntot is the len of the period array in input. If
+	False the fraction is N2/(N1+N2) so we do not consider the number of objects that have not been classified.
+	:return:  fraction of Type1 RRL stars.
+	"""
+
+	classification_array = classify_type12(period, amp, offset=offset, extra_cut=extra_cut, period_range=period_range,
+										   amp_range=amp_range)
+	N2 = np.sum(classification_array == 2)
+	if consider_all:
+		frac = N2 / len(period)
+	else:
+		N1 = np.sum(classification_array == 1)
+		Ntot = N1 + N2
+		frac = N2 / Ntot
+
+	return frac
+
+def f_HASP(period, amp, offset=-0.15, extra_cut=True, period_range_HASP=(0.43,0.48), amp_range_HASP=(0.9,1.45), period_range_type12=(0.43, 0.9), amp_range_type12=None, consider_all=False):
+	"""
+	Estimate the fraction of HASP RRL stars (Bases on the classification by Belokurov+18 (MNRAS.477.1472B) )
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range_HASP: period box to be used to select HASP RRL. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range_HASP: amp box to be used to select HASP RRL. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param period_range_type12: extra cut on period range to be used to estimate type12 RRL. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range_type12: extra cut on amp range to be used to estimate type12 RRL If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param consider_all: if True, the fraction is NHASP/Ntot where Ntot is the len of the period array in input. If
+	False the fraction is NHASP/(N1+N2) so we do not consider the number of objects that have not been classified.
+	:return:  fraction of HASP RRL stars.
+	"""
+
+	NHASP = np.sum(is_HASP(period, amp, offset=offset, period_range=period_range_HASP, amp_range=amp_range_HASP))
+
+	if consider_all:
+		classification_array = classify_type12(period, amp, offset=offset, extra_cut=extra_cut, period_range=period_range_type12,
+										   amp_range=amp_range_type12)
+		Ntot = np.sum( (classification_array == 1) | (classification_array == 1) )
+	else:
+		Ntot=len(period)
+
+	frac = NHASP/Ntot
+
+	return frac
+
+
+	return frac
