63c9db70a89ad5e6f0c8162e0ecada174a914e30
Giuliano Iorio <giuliano.iorio89@gmail.com>
2019-07-03 11:14:00 +0100

    ploth2

--- GAstro/plot.py
+++ GAstro/plot.py
@@ -8,11 +8,16 @@
 #
 #
 ################################################
+from __future__ import division
 from scipy import linalg
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from matplotlib.colors import LogNorm, PowerNorm
+from scipy.stats import binned_statistic_2d as bd2
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def plot_ellipse(mean,covar,ax=None):
 
@@ -22,3 +27,175 @@ def plot_ellipse(mean,covar,ax=None):
 	angle=np.arctan(u[1]/u[0])*(180./np.pi)
 	ell = mpl.patches.Ellipse(mean, v[0], v[1], 180+angle)
 
+def ploth2(x=[],y=[],z=None, statistic='mean', H=None,edges=None,ax=None,bins=100,weights=None, colorbar=True, colorbar_label='',linex=[],liney=[],func=[],xlim=None,ylim=None,xlabel=None,ylabel=None,fontsize=14,cmap='gray_r',gamma=1,invertx=False,inverty=False,interpolation='none',title=None,vmax=None,norm=None,range=None,vmin=None, vminmax_option='percentile',zero_as_blank=True,levels=None,xlogbin=False,ylogbin=False, aspect='equal'):
+	"""
+
+	:param x:
+	:param y:
+	:param z:
+	:param statistic:
+	:param H:
+	:param edges:
+	:param ax:
+	:param bins:
+	:param weights:
+	:param colorbar:
+	:param colorbar_label:
+	:param linex:
+	:param liney:
+	:param func:
+	:param xlim:
+	:param ylim:
+	:param xlabel:
+	:param ylabel:
+	:param fontsize:
+	:param cmap:
+	:param gamma:
+	:param invertx:
+	:param inverty:
+	:param interpolation:
+	:param title:
+	:param vmax:
+	:param norm:
+	:param range:
+	:param vmin:
+	:param vminmax_option:
+	:param zero_as_blank:
+	:param levels:
+	:param xlogbin:
+	:param ylogbin:
+	:param aspect:
+	:return:
+	"""
+
+	if H is None:
+
+		if range is None: range = [[np.min(x), np.max(x)], [np.min(y), np.max(y)]]
+		else: range = range
+
+		if isinstance(bins,float) or isinstance(bins,int):
+			bins_t=[bins,bins]
+			samebin=True
+		else:
+			bins_t=[bins[0],bins[1]]
+			samebin=False
+
+		bins=[[0,],[0,]]
+
+
+
+		if xlogbin:
+			bins[0]=np.logspace(np.log10(range[0][0]),np.log10(range[0][1]),bins_t[0]+1)
+		else:
+			bins[0]=np.linspace(range[0][0], range[0][1],bins_t[0]+1)
+
+		if ylogbin:
+			bins[1]=np.logspace(np.log10(range[1][0]),np.log10(range[1][1]),bins_t[1]+1)
+		else:
+			bins[1]=np.linspace(range[1][0], range[1][1],bins_t[1]+1)
+
+
+
+
+		if z is None:
+			sample=np.vstack([x,y]).T
+			H,edges=np.histogramdd(sample,bins,weights=weights,range=range)
+			xedges,yedges=edges
+			extent=[xedges[0],xedges[-1],yedges[0],yedges[-1]]
+
+
+		elif len(z)==len(x):
+			H, xedges, yedges,_=bd2(x, y, z, statistic=statistic, bins=bins, range=range, expand_binnumbers=False)
+			extent=[xedges[0],xedges[-1],yedges[0],yedges[-1]]
+
+		else:
+			raise ValueError('Z needs to be None or an array with the same length of z and y')
+	else:
+		H=H
+		xedges,yedges=edges
+		extent=[xedges[0],xedges[-1],yedges[0],yedges[-1]]
+
+	if norm is not None:
+		if norm=='max':
+			H=H/np.nanmax(H)
+		elif norm=='tot':
+			H=H/np.nansum(H)
+		elif norm=='maxrows':
+			Hm=np.nanmax(H,axis=0)
+			H=H/Hm
+		elif norm=='totrows':
+			Hm=np.nansum(H,axis=0)
+			H=H/Hm
+		elif norm=='maxcols':
+			Hm=np.nanmax(H,axis=1)
+			H=(H.T/Hm).T
+		elif norm=='totcols':
+			Hm=np.nansum(H,axis=1)
+			H=(H.T/Hm).T
+		elif norm[:10]=='percentile':
+			q=float(norm[10:12])
+			Hm=np.nanpercentile(H,q=q)
+			H=H/Hm
+		else: raise ValueError('norm option %s not recognised (valide values: max, tot, maxcols, maxrows, totcols, totrows)'%str(norm))
+
+	if zero_as_blank:
+		H=np.where(H==0,np.nan,H)
+
+	if ax is not None:
+
+		if vminmax_option=='percentile':
+			if vmax is None: vmaxM=np.nanpercentile(H,q=95)
+			else: vmaxM=np.nanpercentile(H,q=vmax)
+			if vmin is None: vminM=np.nanpercentile(H,q=5)
+			else: vminM=np.nanpercentile(H,q=vmin)
+		elif vminmax_option=='absolute':
+			if vmax is None: vmaxM=np.nanmax(H)
+			else: vmaxM=vmax
+			if vmin is None: vminM=np.nanmin(H)
+			else: vminM=vmin
+		#X,Y=np.meshgrid(xedges,yedges)
+
+
+		if gamma==0: im=ax.imshow(H.T,origin='low',extent=extent, aspect=aspect,cmap=cmap,norm=LogNorm(),interpolation=interpolation,vmax=vmaxM,vmin=vminM)
+		else: im=ax.imshow(H.T,origin='low',extent=extent, aspect=aspect,cmap=cmap,norm=PowerNorm(gamma=gamma),interpolation=interpolation,vmax=vmaxM,vmin=vminM)
+		#if gamma==0: im=ax.pcolormesh(X,Y,H.T, cmap=cmap,norm=LogNorm(),vmax=vmax,vmin=vmin)
+		#else: im=ax.pcolormesh(X,Y,H.T, cmap=cmap,norm=PowerNorm(gamma=gamma),vmax=vmax,vmin=vmin)
+
+		if len(linex)>0:
+			for c in linex:
+				ax.plot([c,c],[yedges[0],yedges[-1]],c='blue',lw=2,zorder=1000)
+
+		if len(liney)>0:
+			for c in liney:
+				ax.plot([xedges[0],xedges[-1]],[c,c],c='blue',lw=2,zorder=1000)
+
+		if len(func)>0:
+			if xlim is not None: xf=np.linspace(xlim[0],xlim[1])
+			else: xf=np.linspace(range[0][0],range[0][1])
+			for f in func:
+				ax.plot(xf,f(xf),c='blue',lw=2,zorder=1000)
+
+		if levels is not None:
+			ax.contour(H.T,origin='lower',extent=extent,levels=levels)
+
+
+		if xlim is not None: ax.set_xlim(xlim)
+		if ylim is not None: ax.set_ylim(ylim)
+
+		if invertx: ax.invert_xaxis()
+		if inverty: ax.invert_yaxis()
+
+		if xlabel is not None: ax.set_xlabel(xlabel,fontsize=fontsize,labelpad=2)
+		if ylabel is not None: ax.set_ylabel(ylabel,fontsize=fontsize,labelpad=2)
+		if title is not None: ax.set_title(str(title),fontsize=fontsize)
+	else:
+		im=None
+
+	if colorbar:
+		divider = make_axes_locatable(ax)
+		cax     = divider.append_axes('right', size='5%', pad=0.05)
+		cbar	= plt.colorbar(im, cax=cax)
+		cbar.ax.set_ylabel(colorbar_label, fontsize=fontsize)
+
+	edges=(xedges,yedges)
+	return H,edges,im
---
+++ build/lib/GAstro/rrl.py
@@ -0,0 +1,231 @@
+################################################
+#
+#
+#
+# Utils for analyse RRL stars
+#
+#
+#
+#
+################################################
+import numpy as np
+
+def _type_1_2_division(period, offset=-0.15):
+	"""
+	Line separating Type1, Type2 RRL in period/amp plane as defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 2)
+	NB. for amp (peak_to_peak_g) from the Gaia DR2 RRL cats use the default offset=-0.15.
+	:param period: rrl period [days]
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B)
+	:return: the amplitude line separing type1 and type2 (type1 amp<return, type2 amp>return)
+	"""
+
+	_C0 = -2.477
+	_C1 = -22.046
+	_C2 = -30.876
+
+	logP = np.log10(period)
+
+	return _C0 + _C1*logP + _C2*logP*logP + offset
+
+def _type12_cut(period, offset=-0.15):
+	"""
+	Extra cut on amp used in Belokurov+18 (MNRAS.477.1472B) to define Type1/Type2 RRL stars (Eq. 1C)
+	NB. for amp (peak_to_peak_g) from the Gaia DR2 RRL cats use the default offset=-0.15.
+	:param period: rrl period [days]
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B)
+	:return:  the amplitude line used to define Type1/Type2  (amp>return the stars are not defined as Type1 or Type2)
+	"""
+
+	_A = 3.75
+	_B = -3.5
+
+	return _A + _B*period + offset
+
+def classify_type12(period, amp, offset=-0.15, extra_cut=True, period_range=(0.43,0.9), amp_range=None):
+
+	"""
+	Taking the period and amplitude clasify the stars as Type1 or Type2 RRLs based on the definition in Belokurov+18 (MNRAS.477.1472B)
+	NB. for amp (peak_to_peak_g) from the Gaia DR2 RRL cats use the default offset=-0.15.
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range: extra cut on period range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range: extra cut on amp range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:return: A 1D array with classication as Type1 (1), Type2 (2) or unclassified (numpy NAN).
+	"""
+
+
+	idx_general = np.ones_like(period, dtype=bool)
+
+	if extra_cut:                     idx_general*=amp<_type12_cut(period,offset)
+	if period_range is not None:      idx_general*=(period>np.min(period_range))&(period<np.max(period_range))
+	if amp_range is not None:         idx_general*=(amp>np.min(amp_range))&(period<np.max(amp_range))
+
+	amp_separation = _type_1_2_division(period, offset)
+
+	idx_type1 = idx_general & (amp<amp_separation)
+	idx_type2 = idx_general & (amp>amp_separation)
+
+
+	return_col = np.zeros_like(period)
+	return_col[idx_type1] = 1
+	return_col[idx_type2] = 2
+	return_col[return_col==0] = np.nan
+
+	return return_col
+
+def is_type1(period, amp, offset=-0.15, extra_cut=True, period_range=(0.43,0.9), amp_range=None):
+
+	"""
+	Check what stars are Type1 RRL stars, following the definition in Belokurov+18 (MNRAS.477.1472B)
+	NB. for amp (peak_to_peak_g) from the Gaia DR2 RRL cats use the default offset=-0.15.
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range: extra cut on period range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range: extra cut on amp range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:return: A boolean array with the same len of period and amp with True where the stars can be classified as Type1 RRL
+	"""
+
+
+	col_type = classify_type12(period, amp, offset, extra_cut, period_range, amp_range)
+
+	return col_type==1
+
+
+def is_type2(period, amp, offset=-0.15, extra_cut=True, period_range=(0.43, 0.9), amp_range=None):
+	"""
+	Check what stars are Type2 RRL stars, following the definition in Belokurov+18 (MNRAS.477.1472B)
+	NB. for amp (peak_to_peak_g) from the Gaia DR2 RRL cats use the default offset=-0.15.
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range: extra cut on period range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range: extra cut on amp range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:return: A boolean array with the same len of period and amp with True where the stars can be classified as Type2 RRL
+	"""
+
+	col_type = classify_type12(period, amp, offset, extra_cut, period_range, amp_range)
+
+	return col_type == 2
+
+def is_HASP(period, amp, offset=-0.15, period_range=(0.43,0.48), amp_range=(0.9,1.45)):
+	"""
+	Check what stars can be considered HASP. based on the definition by Belokurov+18 (MNRAS.477.1472B) Eq.3.
+	In general this can be used to check if the stars are in a given period-amplitude box.
+	NB: remember that the the amp range is corrected for the offset (default value is -0.15), if the offset is not needed put it to 0.
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param period_range: period box to be used to select HASP RRL. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range: amp box to be used to select HASP RRL. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:return: A boolean array with the same len of period and amp with True where the stars are in the given period-amplitude box.
+	"""
+
+	idx_general = np.ones_like(period, dtype=bool)
+	if period_range is not None: idx_general*=(period>np.min(period_range))&(period<np.max(period_range))
+	if amp_range is not None:  idx_general*=(amp>(np.min(amp)+offset))&(amp<(np.max(amp)+offset))
+
+	return idx_general
+
+def f_type1(period, amp, offset=-0.15, extra_cut=True, period_range=(0.43, 0.9), amp_range=None, consider_all=False):
+	"""
+	Estimate the fraction of Type1 RRL stars (Bases on the classification by Belokurov+18 (MNRAS.477.1472B) )
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range: extra cut on period range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range: extra cut on amp range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param consider_all: if True, the fraction is N1/Ntot where Ntot is the len of the period array in input. If
+	False the fraction is N1/(N1+N2) so we do not consider the number of objects that have not been classified.
+	:return:  fraction of Type1 RRL stars.
+	"""
+
+	classification_array=classify_type12(period, amp, offset=offset, extra_cut=extra_cut, period_range=period_range, amp_range=amp_range)
+	N1 = np.sum(classification_array == 1)
+	if consider_all:
+		frac = N1/len(period)
+	else:
+		N2 = np.sum(classification_array==2)
+		Ntot = N1+N2
+		frac = N1 / Ntot
+
+	return frac
+
+
+def f_type2(period, amp, offset=-0.15, extra_cut=True, period_range=(0.43, 0.9), amp_range=None, consider_all=False):
+	"""
+	Estimate the fraction of Type2 RRL stars (Bases on the classification by Belokurov+18 (MNRAS.477.1472B) )
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range: extra cut on period range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range: extra cut on amp range. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param consider_all: if True, the fraction is N2/Ntot where Ntot is the len of the period array in input. If
+	False the fraction is N2/(N1+N2) so we do not consider the number of objects that have not been classified.
+	:return:  fraction of Type1 RRL stars.
+	"""
+
+	classification_array = classify_type12(period, amp, offset=offset, extra_cut=extra_cut, period_range=period_range,
+										   amp_range=amp_range)
+	N2 = np.sum(classification_array == 2)
+	if consider_all:
+		frac = N2 / len(period)
+	else:
+		N1 = np.sum(classification_array == 1)
+		Ntot = N1 + N2
+		frac = N2 / Ntot
+
+	return frac
+
+def f_HASP(period, amp, offset=-0.15, extra_cut=True, period_range_HASP=(0.43,0.48), amp_range_HASP=(0.9,1.45), period_range_type12=(0.43, 0.9), amp_range_type12=None, consider_all=False):
+	"""
+	Estimate the fraction of HASP RRL stars (Bases on the classification by Belokurov+18 (MNRAS.477.1472B) )
+	:param period: rrl period [days]
+	:param amp:  rrl amplitude (peak_to_peak)
+	:param offset: Offset of the amplitude (peak_to_peak) wrt to the function defined in Belokurov+18 (MNRAS.477.1472B) (Eq. 1C)
+	:param extra_cut: if True, apply the extra_cut used in Belokurov+18 (MNRAS.477.1472B) to classify stars
+	:param period_range_HASP: period box to be used to select HASP RRL. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range_HASP: amp box to be used to select HASP RRL. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param period_range_type12: extra cut on period range to be used to estimate type12 RRL. If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param amp_range_type12: extra cut on amp range to be used to estimate type12 RRL If not None, it need to be a tuple with two values. Only stars
+	within these values will be taken into account for the classification.
+	:param consider_all: if True, the fraction is NHASP/Ntot where Ntot is the len of the period array in input. If
+	False the fraction is NHASP/(N1+N2) so we do not consider the number of objects that have not been classified.
+	:return:  fraction of HASP RRL stars.
+	"""
+
+	NHASP = np.sum(is_HASP(period, amp, offset=offset, period_range=period_range_HASP, amp_range=amp_range_HASP))
+
+	if consider_all:
+		classification_array = classify_type12(period, amp, offset=offset, extra_cut=extra_cut, period_range=period_range_type12,
+										   amp_range=amp_range_type12)
+		Ntot = np.sum( (classification_array == 1) | (classification_array == 1) )
+	else:
+		Ntot=len(period)
+
+	frac = NHASP/Ntot
+
+	return frac
+
+
