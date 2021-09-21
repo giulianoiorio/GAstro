e7e2146d1b3214dd00be006e4d0396c326b545a9
iogiul <giuliano.iorio.astro@gmail.com>
2021-09-21 17:02:48 +0200

    We added cutils functions from pycam

--- setup.py
+++ setup.py
@@ -2,41 +2,7 @@
 from __future__ import print_function
 import pip
 import time
-'''
-#Check Cython installation
-print('Checking Ctyhon')
-try:
-    import Cython
-    cyv=Cython.__version__
-    print('OK! (Version %s)'%cyv)
-except:
-    print('Cython is not present, I will install it for you my lord')
-    pip.main(['install','Cython'])
 
-#Check CythonGSL installation
-print('Checking CtyhonGSL')
-try:
-    import cython_gsl
-    print('OK!')
-except:
-    print('Cython is not present, I will install it for you my lord')
-    pip.main(['install','CythonGSL'])
-
-#Check Scipy>1.0 installation
-print('Checking Scipy>1.0')
-try:
-    import scipy
-    scv=scipy.__version__
-    scvl=scv.split('.')
-    if int(scvl[0])>0 or int(scvl[1])>19:
-        print('OK! (Version %s)'%scv)
-    else:
-        print('Version %s too old. I will install the lastest version' % scv)
-        pip.main(['install','scipy'])
-except:
-    print('Scipy is not present, I will install it for you my lord')
-    pip.main(['install','scipy'])
-'''
 
 
 from distutils.core import  setup, Extension
